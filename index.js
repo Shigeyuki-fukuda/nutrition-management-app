899ccbb30742e626f93ee297feabe1ee065067cb
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-13 17:19:23 +0900

    feat: 各食品のclick時にopacityを変更するようにした

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 "use strict";
+var Score = /** @class */ (function () {
+    function Score() {
+    }
+    return Score;
+}());
+var Food = /** @class */ (function () {
+    function Food(element) {
+        this.element = element;
+        element.addEventListener('click', this.clickEventHandler.bind(this));
+    }
+    Food.prototype.clickEventHandler = function () {
+        console.log(this.element.classList);
+        this.element.classList.toggle('bg-gray-300');
+        console.log(this.element.classList);
+    };
+    return Food;
+}());
+var Foods = /** @class */ (function () {
+    function Foods() {
+        this.elements = document.querySelectorAll('.food');
+        this.elements.forEach(function (element) {
+            new Food(element);
+        });
+    }
+    return Foods;
+}());
+var foods = new Foods();
+console.log(foods);
--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 class Score {}
-class Food {}
-class Foods {}
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler.bind(this));
+  }
+  clickEventHandler() {
+    console.log(this.element.classList);
+    this.element.classList.toggle('bg-gray-300');
+    console.log(this.element.classList);
+  }
+}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+
+  constructor() {
+    this.elements.forEach(element => {
+      new Food(element);
+    })
+  }
+}
+const foods = new Foods();
+console.log(foods);
