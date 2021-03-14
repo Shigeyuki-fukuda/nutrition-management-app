d249ad9de43a210924e8549012a77a87c2fca0f4
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2021-03-14 20:25:25 +0900

    feat: ts-loaderを使ってTypeScriptを直接扱うための設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 module.exports = {
-  entry: './dist/main.js',
+  entry: './src/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/, // testはloaderを適用すべきファイルの正規表現のためのキーワード
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  }
 }
